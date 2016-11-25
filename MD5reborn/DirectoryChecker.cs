70b76edc7a278d88144fcddb51966f912924865f
jonathan2266 <jonathan2266@hotmail.be>
2016-11-25 17:24:46 +0100

    initial commit made general program structure

---
+++ MD5reborn/DirectoryChecker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MD5reborn.logger;
+
+namespace MD5reborn
+{
+    public class DirectoryChecker
+    {
+        private string dir;
+        private string unfinishedTag;
+        private Ilogger logger;
+        public DirectoryChecker(string dir, string unfinishedTag, Ilogger logger)
+        {
+            this.dir = dir;
+            this.unfinishedTag = unfinishedTag;
+            this.logger = logger;
+        }
+
+        public List<string> GetUnFinsished()
+        {
+            List<string> unfinished = new List<string>();
+            string[] temp;
+            try
+            {
+                temp = Directory.GetFiles(dir);
+            }
+            catch (Exception)
+            {
+                //log stuff
+                Environment.Exit(1);
+            }
+            for (int i = 0; i < temp.Length; i++)
+            {
+                string temp2 = Path.GetFileName(temp[i]);
+                
+                if (temp[i].Contains(unfinishedTag))
+                {
+                    unfinished.Add(temp[i]);
+                }
+            }
+
+            return unfinished;
+        }
+    }
+}
--- MD5reborn/Program.cs
+++ MD5reborn/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MD5reborn.logger;
 
 namespace MD5reborn
 {
@@ -10,6 +11,25 @@ namespace MD5reborn
     {
         static void Main(string[] args)
         {
+            string directory = args[0];
+
+            Logger logger = new FileLogger();
+
+            string fileUnfinishedTag = "unf";
+            DirectoryChecker dirCheck = new DirectoryChecker(directory, fileUnfinishedTag, logger);
+
+            //startAguments dir
+            //make logger writes down basic info in a txt file like cores/os id etc
+
+            //checklatestmake hash
+
+            //depending on outcome -> fire up and finish files then fire up threadmanager
+
+            //else fire up threadmanager and hash new files
+
+            //fire up diffrent thread for console commands
+
+            //fire up extra thread for displaying data
         }
     }
 }
