a6d53c6d21d244c93df881706b257ea77e34835f
jonathan2266 <jonathan2266@hotmail.be>
2016-11-30 14:17:46 +0100

    forgot file

---
+++ MD5reborn/dataSaver/DataSaver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MD5reborn.logger;
+using MD5reborn.format;
+
+namespace MD5reborn.dataSaver
+{
+    public abstract class DataSaver : IDataSaver, IDisposable
+    {
+        protected Ilogger logger;
+        protected IFormat format;
+        public DataSaver(Ilogger logger, IFormat format)
+        {
+            this.format = format;
+            this.logger = logger;
+        }
+
+        public abstract void Dispose();
+        public abstract void PushData(string text, string hash);
+    }
+}
